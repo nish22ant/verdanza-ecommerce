37a54fb19785484789219f86ed36913314dea997
Nishant Kumar <nknishantkumar378@gmail.com>
2023-03-09 12:00:14 -0500

    initial commit

---
+++ VerdanzaWebParent/VerdanzaBackend/src/main/java/com/verdanza/admin/VerdanzaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.verdanza.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerdanzaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerdanzaBackendApplication.class, args);
+	}
+
+}
---
+++ VerdanzaWebParent/VerdanzaFrontend/src/main/java/com/verdanza/VerdanzaFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.verdanza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerdanzaFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerdanzaFrontendApplication.class, args);
+	}
+
+}
